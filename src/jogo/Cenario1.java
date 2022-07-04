4d2e0766a2de97b05b81cc84cec122954a0adf4d
Luís Henrique <61325756+henrique15775@users.noreply.github.com>
2022-07-04 18:50:02 -0300

    commit2

--- src/jogo/Cenario1.java
+++ src/jogo/Cenario1.java
@@ -7,19 +7,23 @@ public class Cenario1 {
 	
 	private Window janela;
 	private Scene cena;
+	private Jogador jogador;
 	
 	public Cenario1(Window window) {
 		janela = window;
 		cena = new Scene();
 		cena.loadFromFile("C:\\Users\\Administrador\\eclipse-workspace\\jogo\\src\\recursos\\tiles\\Cenario1.scn");
-		
+		jogador = new Jogador(640,350);
 		run();
 	}
 	
 	private void run() {
 		while(true) {
 			cena.draw();
+			jogador.draw();
+			jogador.mover(janela);
 			janela.update();
+			
 		}
 	}
 	
